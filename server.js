7d77bb12912f52577235a53000ec24abc4f46be7
laurencantlin <laurenrose5321@gmail.com>
2018-02-19 18:53:30 -0600

    adds js code

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 var app = express();
 var PORT = 3000;
@@ -8,6 +7,8 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
