c6bf69320587fd3746ca6dad7a80c04010adb200
laurencantlin <laurenrose5321@gmail.com>
2018-02-21 18:49:01 -0600

    testing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,7 +3,7 @@ var friendsData = require("../data/friends");
 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
-       return res.json(friendsData);
+        res.json(friendsData);
     });
 
     app.post("/api/friends", function (req, res) {
